6f6b4f85d5d021f3d81b03bf2e146687193ee554
lumimd <mahdinoorani122@gmail.com>
2026-07-10 03:03:12 +0330

    users management  changed

---
+++ src/pages/UsersManagement.jsx
@@ -0,0 +1,87 @@
+import {
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  CardText,
+} from "reactstrap";
+
+import {
+  Eye,
+  FileText,
+  User,
+  Users,
+  UserX,
+  Heart,
+  Award,
+  Truck,
+  Server,
+  Activity,
+  ShoppingBag,
+  AlertOctagon,
+  MessageSquare,
+  Circle,
+  CheckCircle,
+  XCircle,
+  Mail,
+} from "react-feather";
+
+import StatsHorizontal from "../components/_Global/StatsHorizontal";
+import StatsVertical from "../components/_Global/StatsVertical";
+import SelectReact from "../components/UsersManagement/SelectReact";
+import UsersList from "../components/UsersManagement/invoice/list/index"
+
+
+const SecondPage = () => {
+  return (
+    <div
+      style={{
+        height: "auto",
+        width: "100%",
+        display: "flex",
+        flexFlow: "column",
+      }}
+    >
+      <Row>
+        <Col lg="3" sm="3">
+          <StatsHorizontal
+            icon={<Users size={21} />}
+            color="info"
+            stats="86%"
+            statTitle="تعداد کاربران"
+          />
+        </Col>
+        <Col lg="3" sm="3">
+          <StatsHorizontal
+            icon={<UserX size={21} />}
+            color="warning"
+            stats="1.2gb"
+            statTitle="کاربران با اطلاعات ناقص"
+          />
+        </Col>
+        <Col lg="3" sm="3">
+          <StatsHorizontal
+            icon={<Award size={21} />}
+            color="success"
+            stats="0.1%"
+            statTitle="اساتید"
+          />
+        </Col>
+        <Col lg="3" sm="3">
+          <StatsHorizontal
+            icon={<User size={21} />}
+            color="primary"
+            stats="13"
+            statTitle="دانشجویان"
+          />
+        </Col>
+      </Row>
+      <SelectReact/>
+      <UsersList/>
+    </div>
+  );
+};
+
+export default SecondPage;
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,8 @@
 // ** Reducers Imports
 import layout from "./layout";
 import navbar from "./navbar";
+import invoice from '../components/UsersManagement/invoice/store'
 
-const rootReducer = { navbar, layout };
+const rootReducer = { navbar, layout, invoice};
 
 export default rootReducer;
