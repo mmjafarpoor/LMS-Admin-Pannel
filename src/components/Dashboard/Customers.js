96ccc1f2c7eac8b05c336de1f106a1d8e73e6f5e
lumimd <mahdinoorani122@gmail.com>
2026-07-09 18:36:27 +0330

    dashboard page changed

---
+++ src/components/Dashboard/Customers.js
@@ -0,0 +1,99 @@
+// ** React Imports
+import { useEffect, useState } from 'react'
+
+// ** Third Party Components
+import axios from 'axios'
+import classnames from 'classnames'
+import * as Icon from 'react-feather'
+import Chart from 'react-apexcharts'
+
+// ** Reactstrap Imports
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardHeader,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  UncontrolledDropdown
+} from 'reactstrap'
+
+const Customers = props => {
+  // ** State
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    axios.get('/card/card-analytics/customers').then(res => setData(res.data))
+  }, [])
+
+  // console.log(props)
+  // console.log(data)
+
+  const options = {
+      chart: {
+        toolbar: {
+          show: false
+        }
+      },
+      labels: ['کاربر', 'استاد', 'ادمین', 'مدیر'],
+      dataLabels: {
+        enabled: false
+      },
+      legend: { show: false },
+      stroke: {
+        width: 4
+      },
+      colors: [props.primary, props.warning, props.danger, props.warning]
+    },
+    series = [690, 258, 149, 200]
+
+  const renderChartInfo = () => {
+    return data.listData.map((item, index) => {
+      const IconTag = Icon[item.icon]
+
+      return (
+        <div
+          key={index}
+          className={classnames('d-flex justify-content-between', {
+            'mb-1': index !== data.listData.length - 1
+          })}
+        >
+          <div className='d-flex align-items-center'>
+            <IconTag
+              size={15}
+              className={classnames({
+                [item.iconColor]: item.iconColor
+              })}
+            />
+            <span className='fw-bold ms-75'>{item.text}</span>
+          </div>
+          <span>{item.result}</span>
+        </div>
+      )
+    })
+  }
+
+  return data !== null ? (
+    <Card>
+      <CardHeader className='align-items-end'>
+        <CardTitle tag='h4'>دسته‌ بندی کاربران بر اساس نقش</CardTitle>
+        <UncontrolledDropdown className='chart-dropdown'>
+          
+          <DropdownMenu end>
+            {data.last_days.map(item => (
+              <DropdownItem className='w-100' key={item}>
+                {item}
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </CardHeader>
+      <CardBody>
+        <Chart options={options} series={series} type='pie' height={325} />
+        <div className='pt-25'>{renderChartInfo()}</div>
+      </CardBody>
+    </Card>
+  ) : null
+}
+export default Customers
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,52 +5,103 @@ import {
   CardTitle,
   CardText,
   CardLink,
+  Row,
+  Col
 } from "reactstrap";
 
-const Home = () => {
-  return (
-    <div>
-      <Card>
-        <CardHeader>
-          <CardTitle>Kick start your project 🚀</CardTitle>
-        </CardHeader>
-        <CardBody>
-          <CardText>All the best for your new project.</CardText>
-          <CardText>
-            Please make sure to read our{" "}
-            <CardLink
-              href="https://pixinvent.com/demo/vuexy-react-admin-dashboard-template/documentation/"
-              target="_blank"
-            >
-              Template Documentation
-            </CardLink>{" "}
-            to understand where to go from here and how to use our template.
-          </CardText>
-        </CardBody>
-      </Card>
+import {
+  Eye,
+  FileText,
+  User,
+  Users,
+  UserX,
+  Heart,
+  Award,
+  Truck,
+  Server,
+  Activity,
+  ShoppingBag,
+  AlertOctagon,
+  MessageSquare,
+  Circle,
+  CheckCircle,
+  XCircle,
+  Mail, 
+} from 'react-feather'
 
-      <Card>
-        <CardHeader>
-          <CardTitle>Want to integrate JWT? 🔒</CardTitle>
-        </CardHeader>
-        <CardBody>
-          <CardText>
-            We carefully crafted JWT flow so you can implement JWT with ease and
-            with minimum efforts.
-          </CardText>
-          <CardText>
-            Please read our{" "}
-            <CardLink
-              href="https://pixinvent.com/demo/vuexy-react-admin-dashboard-template/documentation/docs/development/auth"
-              target="_blank"
-            >
-              JWT Documentation
-            </CardLink>{" "}
-            to get more out of JWT authentication.
-          </CardText>
-        </CardBody>
-      </Card>
+import { ThemeColors } from '@src/utility/context/ThemeColors'
+import { useContext } from 'react'
+
+import CardCongratulations from "../components/Dashboard/CardCongratulations";
+import CoursesAnalytics from "../components/Dashboard/coursesAnalytics";
+import Customers from "../components/Dashboard/Customers";
+import StatsHorizontal from "../components/Dashboard/StatsHorizontal";
+import StatsVertical from "../components/Dashboard/StatsVertical";
+
+
+const Home = () => {
+
+  const { colors } = useContext(ThemeColors)
+
+  return (
+    <div style={{height: "auto", width: "100%", display: "flex", flexFlow: "row", justifyContent: "space-between"}}>
+      <div style={{height: "auto", width: "49%", display: "flex", flexFlow: "column"}}> 
+        <div>
+          
+        </div>
+        <CardCongratulations/>
+        <CoursesAnalytics success={colors.success.main}/>
+        <Customers
+          primary={colors.primary.main}
+          warning={colors.warning.main}
+          danger={colors.danger.main}
+        />
+        
+      </div>
+      <div style={{height: "auto", width: "49%", display: "flex", flexFlow: "column"}}>
+        <Row>
+          <Col lg='6' sm='6'>
+            <StatsHorizontal icon={<Users size={21} />} color='info' stats='86%' statTitle='تعداد کاربران' />
+          </Col>
+          <Col lg='6' sm='6'>
+            <StatsHorizontal icon={<UserX size={21} />} color='warning' stats='1.2gb' statTitle='کاربران با اطلاعات ناقص' />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg='6' sm='6'>
+            <StatsHorizontal icon={<Award size={21} />} color='success' stats='0.1%' statTitle='اساتید' />
+          </Col>
+          <Col lg='6' sm='6'>
+            <StatsHorizontal icon={<User size={21} />} color='primary' stats='13' statTitle='دانشجویان' />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg='12' sm='12'>
+            <StatsHorizontal icon={<FileText size={21} />} color='dark' stats='13' statTitle='اخبار و مقالات' />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg='4' sm='4'>
+            <StatsVertical icon={<CheckCircle size={21} />} color='success' stats='0.1%' statTitle='دوره‌های درحال برگزاری' />
+          </Col>
+          <Col lg='4' sm='4'>
+            <StatsVertical icon={<Circle size={21} />} color='info' stats='13' statTitle='کل دوره‌ها' />
+          </Col>
+          <Col lg='4' sm='4'>
+            <StatsVertical icon={<XCircle size={21} />} color='danger' stats='0.1%' statTitle='دوره‌های منقضی شده' />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg='6' sm='6'>
+            <StatsVertical icon={<MessageSquare size={21} />} color='warning' stats='13' statTitle='نظرات' />
+          </Col>
+          <Col lg='6' sm='6'>
+            <StatsVertical icon={<Mail size={21} />} color='warning' stats='13' statTitle='پیام‌ها' />
+          </Col>
+        </Row>
+      </div>
     </div>
+    
   );
 };
 
